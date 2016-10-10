bfa31fe3ee8c8b839c86d6492c50cfbf1e9ebe6b
hong <hong25100@hotmail.com>
2016-10-09 19:10:46 -0700

    alembic migration init

---
+++ alembic/versions/69131b03640b_create_user_table.py
@@ -0,0 +1,33 @@
+"""create user table
+
+Revision ID: 69131b03640b
+Revises: 
+Create Date: 2016-10-08 18:57:56.455709
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '69131b03640b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'users',
+        sa.Column('user_id', sa.String(64), primary_key=True, autoincrement=True),
+        sa.Column('first_name', sa.String(32), nullable=False),
+        sa.Column('last_name', sa.String(32), nullable=False),
+        sa.Column('email', sa.String(255), nullable=True),
+        sa.Column('activated_vehicle', sa.String(8), nullable=True),
+        sa.Column('created_at', sa.DateTime, nullable=True, default=sa.func.now()),
+        sa.Column('updated_at', sa.DateTime, nullable=True, default=sa.func.now()),
+    )
+
+
+def downgrade():
+    op.drop_table('users')
---
+++ alembic/versions/83cf2d6876df_create_access_token_table.py
@@ -0,0 +1,30 @@
+"""create access token table
+
+Revision ID: 83cf2d6876df
+Revises: 69131b03640b
+Create Date: 2016-10-09 16:02:23.401746
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '83cf2d6876df'
+down_revision = '69131b03640b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'access_tokens',
+        sa.Column('access_token', sa.String(255), primary_key=True),
+        sa.Column('user_id', sa.String(64), sa.ForeignKey('users.user_id'), index=True),
+        sa.Column('expires_at', sa.DateTime, nullable=False),
+        sa.Column('issued_at', sa.DateTime, nullable=False)
+    )
+
+
+def downgrade():
+    op.drop_table('access_tokens')
