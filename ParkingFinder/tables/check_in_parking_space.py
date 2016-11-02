96d9a34dfa80d156bd9e8d8467d12fe1e6780642
sky wang <34310696@qq.com>
2016-11-02 13:27:24 -0700

    add parking space (#33)
    * add parking space
    
    * add foreign key
    
    *  add parkinspace
    
    *  fix typo
    
    * remove parkingspace

---
+++ ParkingFinder/tables/check_in_parking_space.py
@@ -0,0 +1,35 @@
+from sqlalchemy import (
+    Column,
+    String,
+    Integer,
+    Float,
+    DateTime,
+)
+
+from ParkingFinder.tables.base import Base
+
+
+class CheckInParkingSpace(Base):
+    __tablename__ = 'check_in_parking_space'
+
+    user_id = Column(String(64), ForeignKey('users.user_id'), primary_key=True)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    level = Column(Integer, nullable=True)
+    description = Column(String(500), nullable=True)
+
+    def __repr__(self):
+        return 'user_id: {}, ' \
+               'latitude: {}, ' \
+               'longitude: {}, ' \
+               'created_at: {}, '\
+               'level: {}, ' \
+               'description: {}'.format(
+                self.space_id,
+                self.latitude,
+                self.longitude,
+                self.created_at,
+                self.level,
+                self.description,
+        )
---
+++ ParkingFinder/tables/real_time.py
@@ -0,0 +1,38 @@
+from sqlalchemy import (
+    Column,
+    String,
+    Float,
+    DateTime,
+)
+
+from ParkingFinder.tables.base import Base
+
+
+class RealTime(Base):
+    __tablename__ = 'real_time'
+
+    user_one_id = Column(String(64), ForeignKey('users.user_id'))
+    user_two_id = Column(String(64), ForeignKey('users.user_id'))
+    latitude_one = Column(Float, nullable=False)
+    longitude_one = Column(Float, nullable=False)
+    latitude_two = Column(Float, nullable=False)
+    longitude_two = Column(Float, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+
+    def __repr__(self):
+        return 'user_one_id: {}, ' \
+               'latitude_one: {}, ' \
+               'longitude_one: {}, ' \
+               'user_two_id: {}, ' \
+               'latitude_two: {}, ' \
+               'longitude_two: {}, ' \
+               'created_at: {} '.format(
+                self.user_one_id,
+                self.latitude_one,
+                self.longitude_one,
+                self.user_two_id,
+                self.latitude_two,
+                self.longitude_two,
+                self.created_at,
+        )
