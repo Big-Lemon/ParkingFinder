a7e4019a30f7a20f973c74b423755db03c4182ab
DirecteverZF <AdrianFeng@users.noreply.github.com>
2016-11-01 19:31:03 -0700

    update the user service and for view purpose only (#29)
    * update the user service and for view purpose only
    
    * now handle the case if vehicle is not in the user's list
    
    * user service update continue

--- ParkingFinder/services/user.py
+++ ParkingFinder/services/user.py
@@ -9,12 +9,14 @@ from ParkingFinder.repositories.user_repository import (
     UserNotFound,
     UserRepository,
 )
+from ParkingFinder.repositories.vehicle_repository import VehicleRepository
 from clay import config
 
 logger = config.get_logger('user')
 
 
-
+# the correct flow is to 1.register user first then 2.register a car and finally 3.activate a car
+# to avoid code redundancy, there will be no condition implemented such that each step calls its previous step inside
 class UserService(object):
 
     @classmethod
@@ -46,31 +48,99 @@ class UserService(object):
 
     @staticmethod
     @coroutine
-    def register(user, vehicle=None):
+    def register(user):
         """
-        Register a new user if not exist
+        Register a new user if not exist and register a vehicle for the user
+        this have checked if the car exist or not
 
         :param User user:
-        :param Vehicle vehicle:
-        :return:
+        :return: User Updated User
         """
         user.validate()
-        if vehicle:
-            vehicle.validate()
 
         _user = yield UserRepository.upsert(user=user)
 
         raise Return(_user)
 
-        # TODO if vehicle not None, add to vehicle repository
+    @staticmethod
+    @coroutine
+    def register_vehicle(user_id, vehicle):
+        """
+        register a vehicle for the user and this have checked if the car exist or not
+
+        :param String user_id:
+        :param Vehicle vehicle:
+        :raise: NotFound
+        :return: Vehicle vehicle: inserted vehicle
+        """
+        vehicle.validate()
+        try:
+            user = yield UserRepository.read_one(user_id)
+            user_vehicles = yield VehicleRepository.retrieve_vehicle_by_user(user_id=user.user_id)
+            exist = False
+            for current_vehicle in user_vehicles:
+                if current_vehicle.plate == vehicle.plate:
+                    _vehicle = current_vehicle
+                    exist = True
+                    break
+            if not exist:
+                _vehicle = VehicleRepository.insert_registered_vehicle(user_id=user.user_id, vehicle=vehicle)
+        except NoResultFound:
+            raise NotFound
+
+        raise Return(_vehicle)
+
 
     @staticmethod
     @coroutine
     def get_user_detail(user_id):
+        """
+        Return the user information in detail with registered car information
+
+        :param String user_id:
+        :return: User user:
+        """
         try:
             user = yield UserRepository.read_one(user_id=user_id)
-            # TODO read vehicles from vehicle repository and assign to user entity
+            user_vehicles = yield VehicleRepository.retrieve_vehicle_by_user(user_id=user_id)
+            user.owned_vehicles = user_vehicles
         except NoResultFound:
             raise NotFound
 
         raise Return(user)
+
+    @staticmethod
+    @coroutine
+    def activate_vehicle(user_id, vehicle_plate):
+        """
+        activate an vehicle of a user
+        if user want to activate a car that he doesn't own then he won't be allowed to do so
+        In order to avoid redundancy, This implementation will not help user to register car here
+        So He must register first in order to activate it so the return value can be
+        an indicator since if its activated vehicle has been changed => activate successfully
+        otherwise activation failure means call register function first
+
+        :param String user_id:
+        :param String vehicle_plate:
+        :return: updated user:
+        """
+        try:
+            user = yield UserRepository.read_one(user_id)
+            user_vehicles = yield VehicleRepository.retrieve_vehicle_by_user(user_id=user.user_id)
+            exist = False
+            for current_vehicle in user_vehicles:
+                if current_vehicle.plate == vehicle_plate:
+                    exist = True
+                    break
+            if exist and (user.activated_vehicle != vehicle_plate):
+                user.activated_vehicle = vehicle_plate
+                _user = yield UserRepository.upsert(user=user)
+            else:
+                _user = user
+        except NoResultFound:
+            raise NotFound
+
+        raise Return(_user)
+
+
+
