b3fae1a9215596224c5508e7284c80b88bcf2fc7
Maolei Tang <tangmaolei@tangmaoleideMacBook-Pro.local>
2016-11-03 04:19:33 -0700

    rebase

--- tests/mappers/check_in_parking_space_mapper_test.py
+++ tests/mappers/check_in_parking_space_mapper_test.py
@@ -4,13 +4,15 @@ from ParkingFinder.mappers import parking_space_mapper as module
 def test_mapper():
 
     parking_space = module.ParkingSpace.get_mock_object()
+    params = parking_space.to_primitive()
+    del params['level'], params['description']
+    parking_space = module.ParkingSpace(params)
+
     model = module.CheckInParkingSpace(
         user_id=parking_space.user_id,
         latitude=parking_space.latitude,
         longitude=parking_space.longitude,
         created_at=parking_space.created_at,
-        level=parking_space.level,
-        description=parking_space.description,
     )
     entity = module.CheckInParkingSpaceMapper.to_entity(model)
     assert parking_space == entity
